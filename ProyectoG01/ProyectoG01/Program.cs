ac936c01d7543ed364d53f88d26574ec7396890d
nanxiqin <nanxiqin@ucm.es>
2022-03-16 17:38:20 +0100

    Nueva modificación para hacer pull

--- ProyectoG01/ProyectoG01/Program.cs
+++ ProyectoG01/ProyectoG01/Program.cs
@@ -7,6 +7,7 @@ namespace ProyectoG01
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Nueva linea añadida");
         }
     }
 }
